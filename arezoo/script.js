c7c492fcd06dfa10aa650107b635985f865373f6
arezoofaraji <arezoo.faraji66@gmail.com>
2020-08-05 15:05:15 +0200

    6Q done-arezoo

---
+++ arezoo/script.js
@@ -0,0 +1,21 @@
+let totalPrice = 6450;
+let cost = 15;
+let weekInyear = 4 * 12;
+// q1
+console.log("tickets were sold: " + totalPrice / cost);
+
+// q2
+console.log("sylvia salary every year is: " + weekInyear * 500 + "$");
+
+// q3
+console.log(17 / 3);
+
+// q4
+let a = 4.75;
+console.log(4 * a);
+
+// q5
+console.log(5 + 6 + 7);
+
+// q6
+console.log(5 * 5);
