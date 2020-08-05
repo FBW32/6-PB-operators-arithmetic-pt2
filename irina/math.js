0b8c239550d0893642dc0afb8a61b791950289d2
Irina <irinagmarques@hotmail.com>
2020-08-05 16:01:55 +0200

    solution

---
+++ irina/math.js
@@ -0,0 +1,35 @@
+// Programming Basics: Arithmetic Operators (Math Problems)
+// These tasks are aimed at making you more comfortable with solving math problems using JS. Print each of your results to the console.
+
+// Basic Arithmetic Problems
+
+// Q1. In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many tickets were sold?
+let sells = 6450;
+let ticket = 15;
+console.log(sells / ticket); // 430
+
+// Q2. Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+let income = 500;
+let months = 12;
+console.log(500 * 12);
+
+// Percentage
+
+// Q3. Calculate the percentage of 17/30. Expected output: number%
+let num = 17/30;
+let per = 100;
+console.log(num*per);  // 56.666666666666664%
+
+// Geometry Formulas
+
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+let side = 4.75;
+console.log(4 * side); // 19
+
+// Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+let perimeter = 5 + 6 + 7;
+console.log(perimeter); // 18
+
+// Q6. Calculate the area of a square. Each side is 5cm.
+let sides = 5;
+console.log(sides**2); // 25
