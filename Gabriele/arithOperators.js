e5777d1892e767e89b8b1bccbabe8d249d0e1ead
Gabriele Pepe <gabriele.pepe@gmail.com>
2020-08-05 16:14:16 +0200

    exercises completed

---
+++ Gabriele/arithOperators.js
@@ -0,0 +1,43 @@
+// Q1. In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many tickets were sold?
+
+let totalMoney = 6450;
+let ticketsCost = 15;
+console.log(totalMoney / ticketsCost);
+console.log("430 tickets were sold.")
+
+// Q2. Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+
+let WeekIncome = 500;
+let weeksInYear = 52;
+console.log(WeekIncome * weeksInYear);
+console.log("Sylvia makes 26000 dollars every year.");
+
+
+// Q3. Calculate the percentage of 17/30. Expected output: number%
+
+console.log(17 * 100 / 30);
+console.log("The percentage is 56.6%.");
+
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+
+let sideSize = 4.75;
+let numberOfSides = 4;
+let squarePerimeter = sideSize * 4;
+console.log(sideSize * numberOfSides);
+console.log("The perimeter of the square is 19");
+
+// Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+
+let side1 = 5;
+let side2 = 6;
+let side3 = 7;
+let perimeter = side1 + side2 + side3;
+console.log(side1 + side2 + side3);
+console.log("The perimeter of this triangle is 18");
+
+// Q6. Calculate the area of a square. Each side is 5cm.
+
+let side = 5;
+let area = side*side;
+console.log(side * side);
+console.log("The area of this square is 25cm");
