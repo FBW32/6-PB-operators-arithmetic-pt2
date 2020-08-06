5d0d71b312b9d92b95773268688b5f4b98f2ace9
frausafiye <s_gulenc@outlook.com>
2020-08-06 13:38:54 +0200

    added solutions

---
+++ exercise-6.js
@@ -0,0 +1,15 @@
+//1
+console.log(6450 / 15);
+//2
+console.log(500 * 52);
+//3
+console.log(17 / 100 + "%");
+//4
+let periSquare = 4.75 * 4;
+console.log(periSquare);
+//5
+let periTriangle = 5 + 6 + 7;
+console.log(periTriangle);
+//6
+let areaSquare = 5 * 5;
+console.log(areaSquare);
