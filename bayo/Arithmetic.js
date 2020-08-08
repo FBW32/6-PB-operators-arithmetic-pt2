53af036591e55b77d0209ec464974127edee96c2
Abayomi <mrbayus2014@gmail.com>
2020-08-08 11:10:11 +0200

    arithmetic

---
+++ bayo/Arithmetic.js
@@ -0,0 +1,38 @@
+//05.08.2020
+//QUESTION 1
+//In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many tickets were sold?
+let totalSales = 6450;
+let ticket = 15;
+console.log(totalSales / ticket);
+
+// QUESTION 2
+// Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+let salaryPerWeek = 500;
+let weeksPerYear = 52;
+console.log(salaryPerWeek * weeksPerYear);
+
+// QUESTION 3 
+//percentage
+//Calculate the percentage of 17/30. Expected output: number%
+console.log(17 % 30);
+
+//Geometry Formulas
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+
+let perimeter = 4;
+let eachSide = 4.75;
+console.log(`Perimeter: ${perimeter * eachSide}`);
+
+//Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+
+let side1 = 5;
+let side2 = 6;
+let side3 = 7;
+console.log(`${side1 + side2 + side3}`);
+
+
+// Q6. Calculate the area of a square. Each side is 5cm.
+
+let allSide = 5;
+let totalSide = 4;
+console.log(`${allSide * totalSide}`);
