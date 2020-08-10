494fa8786aa1c76c6a1b0e237600837a80314a91
bartxs <b.soltysik90@gmail.com>
2020-08-10 14:52:54 +0200

    Exercise 6

---
+++ Bartosz/java.js
@@ -0,0 +1,28 @@
+// Basic Arithmetic Problems
+// Q1. In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many tickets were sold?
+let sells = 6450;
+let ticketCost = 15;
+console.log(sells / ticketCost); // 430
+
+// Q2. Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+let sylwiaIncome = 500;
+let month = 12;
+console.log(sylwiaIncome * month); // 6000
+
+// Percentage
+// Q3. Calculate the percentage of 17/30. Expected output: number%
+let percentage = ((17/30) * 100);
+console.log(percentage); // 56.666666666666664
+
+//Geometry Formulas
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+let side = 4.75;
+console.log(4*side); // 19
+
+// Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+let perimeter = 5 + 6 + 7;
+console.log(perimeter); // 18
+
+// Q6. Calculate the area of a square. Each side is 5cm.
+let area = (5*5);
+console.log(area); // 25
