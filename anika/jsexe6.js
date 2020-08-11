716e11017f48fe90aa9d4e1d0520587544bf867f
anika555 <ak15@live.de>
2020-08-11 07:23:01 +0200

    Exercise 6

---
+++ anika/jsexe6.js
@@ -0,0 +1,47 @@
+// ***************Exercise 6**************
+
+// Q1. In one night, a movie theater sells tickets for 6450 dollars.
+// Each ticket costs 15 dollars.
+// How many tickets were sold?
+
+let sumSales = 6450;
+let priceEach = 15;
+
+console.log(sumSales / priceEach); // 430
+// 430 tickets where sold.
+
+// Q2. Sylvia's income is 500 dollars per week.
+// How much does Sylvia make every year?
+
+console.log(500 * 52); // 26,000
+// Sylvia makes 26,000 per year.
+
+// **Percentage**
+
+// Q3. Calculate the percentage of 17/30. Expected output: number%
+// (with string concatenation)
+
+let resultQ3 = (17 * 30) / 100 + "%";
+console.log(resultQ3); // 5.1%
+
+let resultQ3Try = `${(17 * 30) / 100}%`;
+console.log(resultQ3Try); // 5.1%
+
+// **Geometry Formulas**
+
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+
+console.log(4.75 * 4); // 19
+// The square has a perimeter of 19cm.
+
+// Q5. Calculate the perimeter of a triangle.
+// Assume the length of the sides are 5cm, 6cm, 7cm.
+
+console.log(5 + 6 + 7); // 18
+// The perimeter of the triangle is 18cm.
+
+// Q6. Calculate the area of a square. Each side is 5cm.
+
+console.log(5 ** 2);
+// 25
+// The area of the square is 25cm.
